a178372169d9a93f90ad2d8c4347a201467ebfc7
joel-wowz <josephlafluer@hotmail.com>
2020-04-07 02:42:06 +0000

    messed around with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
   validates_uniqueness_of :email
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => {:case_sensitive => false}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
  before(:each) do
-  @user = User.new(first_name: 'big', last_name: 'daddy',email: 'TEST@TEST.COM', password: 'test123', password_confirmation: 'test123')
-  end
+  @user = User.new(first_name: 'big', last_name: 'daddy',email: 'test@test.com', password: 'test123', password_confirmation: 'test123')
+end
 
 describe 'Validations' do
  it 'returns valid with a name' do
@@ -16,48 +16,38 @@ describe 'Validations' do
   @user.password_confirmation = 'swag'
   expect(@user).to_not be_valid
   end
+
+
+  it 'should not work with non-unique emails' do
+    user = User.create!(first_name: 'big', last_name:'dADDY', email: 'tEsT@tEsT.cOm', password:'test12',password_confirmation:'test12')
+    expect(@user).to_not be_valid
+  end
+end
   
-
-
-
-end 
-
 
 describe '.authenticate_with_credentials' do
    it 'should login in sucessful cred' do
     @user.save
     auth = @user.authenticate_with_credentials(@user.email, @user.password)
+    expect(auth).to eql(@user)
+   end
 
-  end
+  it 'should not login with invalid creds' do
+    @user.save
+    failed_attempt = @user.authenticate_with_credentials(@user.email, @user.password = 'lol')
+   end
 
   it.skip 'should auth after user adds spaces' do
     @user.save
-    add_space = User.authenticate_with_credentials(@user.email = ' TEST@TEST.COM ', @user.password)
-    expect(auth)
+    add_space = User.authenticate_with_credentials(@user.email = ' TEST@teSt.COM ', @user.password)
     expect(add_space).to eql(@user)
-  end
-
- 
-
+    end
+    
+    
   it.skip 'should not care about case-sens' do
     @user.save
-    case_sens = User.authenticate_with_credentials(@user.email = 'teSt@teSt.cOm', @user.password)
+    case_sens = User.authenticate_with_credentials(@user.email = 'TEST@teSt.cOm', @user.password)
     expect(case_sens).to eql(@user)
+    end
   end
-
-  it.skip 'should not allow a password with less then 4 chars' do
-  @user.password = 'Testa'
-  @user.password_confirmation = 'Testa'
-  expect(@user).to_not be_valid
-end
-
-it 'should not work with non-unique emails' do
-    user = User.create(first_name: 'big', last_name:'dADDY', email: 'tEsT@tEsT.cOm', password:'test12',password_confirmation:'test12')
-    expect(@user).to_not be_valid
-  end
-
-
-end
-
-
-end
+ end
